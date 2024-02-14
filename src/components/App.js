dd624bf54da7521b19913e6bd96d4248998d1af7
EVELYNKINYA <evelynkinya156@gmail.com>
2024-02-14 09:10:46 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,21 @@ import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <Router>
+      <div>
+        <NavLink />
+        <Route path="/" exact component={Home} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/directors" component={Directors} />
+        <Route path="/actors" component={Actors} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
